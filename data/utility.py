c86a27c66caf25d801f9481a52f7b30175b75697
Kyle Mills <kyle@kylemills.net>
2018-06-15 12:05:13 -0400

    updated to optionally return dict of header objects

--- data/utility.py
+++ data/utility.py
@@ -2,7 +2,7 @@ from uuid import uuid4
 import logging
 import numpy as np
 
-def excel_to_array(s_, header_rows=1):
+def excel_to_array(s_, header_rows=1, return_header_dict=False):
     s_ = s_.rstrip().lstrip()
     tmpfile = "/tmp/{}".format(uuid4())
     with open(tmpfile, 'w') as F:
@@ -13,8 +13,13 @@ def excel_to_array(s_, header_rows=1):
         logging.warning("Not all elements in the first line were interpreted as strings.  Are you sure the default header_rows=1 is what you want?")
     a = a[header_rows:,:]
     header = np.genfromtxt(tmpfile, delimiter="\t", skip_footer=len(a), dtype=np.str)
-
-    return header, a
+    if return_header_dict:
+        hd = {}
+        for i, head in enumerate(header):
+            hd[head] = i
+        return header, a, hd
+    else:
+        return header, a
 
 def spreadsheet_to_array(*args, **kwargs):
     return excel_to_array(*args, **kwargs)
