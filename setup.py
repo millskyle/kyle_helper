8880e7aea26fe6b2446617c5527e2adacd2ad1d8
Kyle Mills <kyle@kylemills.net>
2018-06-15 10:06:45 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(name='kyle_helper',
-      version='1.0.03',
+      version='1.1.01',
       description='Helper functions to help Kyle',
       author='Kyle Mills',
       author_email='kyle.mills@uoit.net',
-      packages=['latex',],
+      packages=['latex','ednn'],
       scripts=['scripts/update_result'],
       install_requires=[
          'inflect',
