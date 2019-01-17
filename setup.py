0052abbb108c159c0b74b0a37d9d6ad32ddd018c
Kyle Mills <kyle.mills@uoit.net>
2019-01-17 14:05:25 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(name='kyle_helper',
-      version='1.2.07',
+      version='1.3.0',
       description='Helper functions to help Kyle',
       author='Kyle Mills',
       author_email='kyle.mills@uoit.net',
-      packages=['latex','dnn', 'data', 'render'],
+      packages=find_packages(),#,['kyle_helper','latex','dnn', 'data', 'render'],
       scripts=['scripts/update_result'],
       install_requires=[
          'inflect',
